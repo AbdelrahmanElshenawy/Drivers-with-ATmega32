fbb29beae75ce265c4cb3dd585fd5d5af69d7201
Abdelrahman <abdelrahman.elshenawy21@gmail.com>
2023-04-23 04:48:27 +0200

    This commit for adding GPIO driver for ATmega32

---
+++ hal_gpio/std_libraries.h
@@ -0,0 +1,17 @@
+/*=================================================================================
+*   @file       std_libraries.h
+*   @brief		Defined libraries
+*   @details    std_libraries.h File 
+				This file contains the defined libraries
+*   @author     Abdelrahman Ahmed Elshenawy 
+===================================================================================*/
+
+#ifndef STD_LIBRARIES_H
+#define STD_LIBRARIES_H
+
+#include <stdio.h>
+#include <stdint.h>
+
+
+#endif /*ifndef STD_LIBRARIES_H*/
+
